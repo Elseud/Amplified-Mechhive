c481bd4e5e98290d5d38f15be10d344a2b766496
SmArtKar <smart.karpov@gmail.com>
2022-12-06 22:52:57 +0300

    Remade everything to work with Athena Framework

--- Source/AmplifiedMechhive/HarmonyPatches.cs
+++ Source/AmplifiedMechhive/HarmonyPatches.cs
@@ -18,27 +18,6 @@ namespace AmplifiedMechhive
             harmony.PatchAll();
         }
 
-        /*
-        [HarmonyPatch(typeof(StunHandler), "Notify_DamageApplied")]
-        public static class ThingWithComps_PreApplyDamage
-        {
-            static bool Prefix(StunHandler __instance, ref DamageInfo dinfo)
-            {
-                if (!(__instance.parent is Pawn))
-                    return true;
-
-                Pawn pawn = __instance.parent as Pawn;
-
-                foreach (HediffComp_EMPShield shield in pawn.health.hediffSet.hediffs.OfType<HediffWithComps>().SelectMany(hediff => hediff.comps).OfType<HediffComp_EMPShield>())
-                {
-                    if (shield.Notify_DamageApplied(ref dinfo))
-                        return false;
-                }
-
-                return true;
-            }
-        }
-
         [HarmonyPatch(typeof(DamageWorker_AddInjury), "ApplyToPawn")]
         public static class DamageWorker_AddInjury_PreApplyToPawn
         {
@@ -57,16 +36,8 @@ namespace AmplifiedMechhive
                     }
                 }
 
-                if (dinfo.Instigator is Pawn)
-                {
-                    Pawn instigator = dinfo.Instigator as Pawn;
-                    Hediff_VicarBuff buff = instigator.health.hediffSet.GetFirstHediffOfDef(AM_DefOf.AM_VicarBuff) as Hediff_VicarBuff;
-                    if (buff != null)
-                        dinfo.SetAmount(dinfo.Amount * buff.damageMultiplier);
-                }
-                
                 return true;
             }
-        }*/
+        }
     }
 }
